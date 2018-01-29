39baee33a92ed746dd47450480029a46738f82a9
Rand Hidayah <rgh2124@columbia.edu>
2018-01-29 14:27:46 -0500

    first commit

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+print("Hello Again!")
+print("I like typing this")
+print("This is so fun")
+print('Yay! Printing')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this')
+
+print("Hi!")
